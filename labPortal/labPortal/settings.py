5c8480c57140cb529ac3e99e896c26c7441a5787
Subhash Bhalerao <csitstudentsguide@gmail.com>
2024-08-06 15:37:47 +0530

    Change allowed_host and static_root setting

--- labPortal/labPortal/settings.py
+++ labPortal/labPortal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ubmjtudekoc9g2+w)uun7(0*etm0p@imm$bh_28^^4_95&j2zg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sirpl1-31-linux.local']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # The absolute path to the directory where static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional locations the staticfiles app will traverse
 STATICFILES_DIRS = [
