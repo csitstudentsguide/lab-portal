7b1b4f4d237735bc5f32d131585dcbabfd31829f
Subhash Bhalerao <csitstudentsguide@gmail.com>
2024-08-07 14:03:02 +0530

    Add database setting for MariaDB server

--- labPortal/labPortal/settings.py
+++ labPortal/labPortal/settings.py
@@ -75,12 +75,27 @@ WSGI_APPLICATION = 'labPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "labportal",
+        "USER": "lpadmin",
+        "PASSWORD": "lpadmin@123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+    }
+}
+
+
 
 
 # Password validation
