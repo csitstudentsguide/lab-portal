9ec73c400e3f576da74cae3b7e6c6e5e1f4358a5
Subhash Bhalerao <csitstudentsguide@gmail.com>
2024-08-08 13:46:03 +0530

    Add view, url link for lab-rules page

--- labPortal/labPortalHome/urls.py
+++ labPortal/labPortalHome/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lab_portal_home, name=''),
+    path('lab-rules', views.lab_rules, name='lab-rules'),
 ]
--- labPortal/labPortalHome/views.py
+++ labPortal/labPortalHome/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def lab_portal_home(request):
     return render(request, 'labPortalHome/lab_portal_home.html')    
+
+def lab_rules(request):
+    return render(request, 'labPortalHome/lab-rules.html')
